82f7f1563cfb10ba59a4bc3ec7ca828e1b5eab1d
Chokeumnuay Khowsakool <chokelive@gmail.com>
2023-01-20 23:42:43 +0700

    Update for FT991

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+
+// V1.1.0
+// - Add RX Tune to VHOA for support another radio rxFT-911
